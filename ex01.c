e9eb3055f76983ae44ddfb4da6862b1c4ce78653
youneswolf <younesbellakrid@gmail.com>
2023-04-27 10:45:58 +0100

    ex01

---
+++ ex01.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+	
+}
+int	main(void)
+{
+	int	number;
+	int	*p;
+	int	**p2;
+	int	***p3;
+	int	****p4;
+	int	*****p5;
+	int	******p6;
+	int	*******p7;
+	int	********p8;
+	int	*********p9;
+	p = &number;
+	p2 = &p;
+	p3 = &p2;
+	p4 = &p3;
+	p5 = &p4;
+	p6 = &p5;
+	p7 = &p6;
+	p8 = &p7;
+	p9 = &p8;
+	number = 1337;
+	ft_ultimate_ft(p9);
+	
+	return (0);
+}
